4f82c017eb45e055d9b3b05a082d015bc8ce4d1c
Zalpcore <z7705201@gmail.com>
2025-01-17 20:43:45 +0200

    Add functionality in FarGalaxy & constants to get opening crawl by API.

--- src/componenets/FarGalaxy.jsx
+++ src/componenets/FarGalaxy.jsx
@@ -1,8 +1,24 @@
-import {openingCrawl} from "../utils/constants.js";
+import {link_api} from "../utils/constants.js";
+import {useEffect, useState} from "react";
 
 const FarGalaxy = () => {
+    const [openingCrawl, setOpeningCrawl] = useState('');
+    let id = Math.floor((Math.random() * 6) + 1);
+    const getOpeningCrawl = async () => {
+        const response = await fetch(`${link_api}/${id}`);
+        const data = await response.json();
+        setOpeningCrawl({
+            textCrawl: data.opening_crawl
+        });
+
+    };
+
+    useEffect(() => {
+        getOpeningCrawl();
+    }, [])
+
     return (
-        <p className="farGalaxy">{openingCrawl}</p>
+        <p className="farGalaxy">{openingCrawl.textCrawl}</p>
     );
 };
 
