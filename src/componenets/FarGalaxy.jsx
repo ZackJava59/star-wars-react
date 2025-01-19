9b59e653e8bf858450adc2256d2056d05cc545ea
Zalpcore <z7705201@gmail.com>
2025-01-20 00:30:14 +0200

    Add spinner in FarGalaxy

--- src/componenets/FarGalaxy.jsx
+++ src/componenets/FarGalaxy.jsx
@@ -3,14 +3,25 @@ import {useEffect, useState} from "react";
 
 const FarGalaxy = () => {
     const [openingCrawl, setOpeningCrawl] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     let id = Math.floor((Math.random() * 6) + 1);
     const getOpeningCrawl = async () => {
-        const response = await fetch(`${link_api}/${id}`);
-        const data = await response.json();
-        setOpeningCrawl({
-            textCrawl: data.opening_crawl
-        });
-
+        setIsLoading(true);
+        try {
+            const response = await fetch(`${link_api}/${id}`);
+            if (!response.ok) {
+                throw new Error(`Error loading Data`);
+            }
+            const data = await response.json();
+            setOpeningCrawl({
+                textCrawl: data.opening_crawl
+            });
+        } catch (e) {
+            setError(e.message);
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     useEffect(() => {
@@ -18,7 +29,23 @@ const FarGalaxy = () => {
     }, [])
 
     return (
-        <p className="farGalaxy">{openingCrawl.textCrawl}</p>
+        <div>
+            {isLoading && (
+                <div className="spinner-border text-primary" role={'status'}>
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            )}
+            {!isLoading && error && (
+                <div className="alert alert-danger">
+                    {error.message}
+                </div>
+            )}
+            {!isLoading && (
+                <div>
+                    <p className="farGalaxy">{openingCrawl.textCrawl}</p>
+                </div>
+            )}
+        </div>
     );
 };
 
