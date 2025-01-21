49adea6d952196dd41289ad89aab5eb4dd6650bb
Zalpcore <z7705201@gmail.com>
2025-01-21 18:35:36 +0200

    Activating header menu, props drilling example

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import Header from "./componenets/Header.jsx";
 import Footer from "./componenets/Footer.jsx";
 import Main from "./componenets/Main.jsx";
+import {useState} from "react";
+import {navItems} from "./utils/constants.js";
 
 function App() {
 
+    const [page, setPage] = useState(navItems[0]);
+
     return (
         <div className={'container-fluid'}>
-            <Header/>
-            <Main/>
+            <Header changePage={setPage}/>
+            <Main page={page}/>
             <Footer/>
         </div>
     )
