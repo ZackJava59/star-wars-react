3f453d7853de99abc19724c08565e8b13cfab866
Zalpcore <z7705201@gmail.com>
2025-01-24 00:04:46 +0200

    add-contact-form-in-Contact

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -1,7 +1,59 @@
 const Contact = () => {
+
+
+
+
+
+
+
+
+
     return (
         <div>
-            Contact
+            <div className='container'>
+                <div className="row">
+                    <div className="col-25">
+                        <label>First Name</label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-25">
+                        <label htmlFor="lname">Last Name</label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-25">
+                        <label htmlFor="country">Country</label>
+                    </div>
+                    <div className="col-75">
+                        <select id="country" name="country">
+                            <option value="australia">Australia</option>
+                            <option value="canada">Canada</option>
+                            <option value="usa">USA</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-25">
+                        <label htmlFor="subject">Subject</label>
+                    </div>
+                    <div className="col-75">
+                        <textarea id="subject" name="subject" placeholder="Write something.."
+                                  style={{height: '200px'}}></textarea>
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <input type="submit" value="Submit"/>
+                </div>
+
+            </div>
         </div>
     );
 };
