eb7b83a67433707de6f7f40a9c9fa17784ddcd51
Zalpcore <z7705201@gmail.com>
2025-01-24 12:42:14 +0200

    Implementing tailWind

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -2,7 +2,7 @@ import Navigation from "./Navigation.jsx";
 
 const Header = ({changePage}) => {
     return (
-        <header className="rounded-top-4p">
+        <header className={'rounded-t-2xl bg-grey-color'}>
             <Navigation changePage={changePage}/>
             <h1 className="text-center fs-1 py-3">Luke Skywalker</h1>
         </header>
