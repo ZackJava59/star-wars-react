6847525d2c0cf351c0e9e641f6040f775462f2ec
Zalpcore <z7705201@gmail.com>
2025-01-28 12:07:22 +0200

    add functionality and contact form to Contacts

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -1,60 +1,60 @@
+import '../contact.css'
+import {link_api, period_month} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+
 const Contact = () => {
 
+    const [planets, setPlanets] = useState(['Wait...']);
 
+    const getPlanetList = async () => {
+        try {
+            const response = await fetch(`${link_api}/v1/planets`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch planet list');
+            } else {
+                const data = await response.json();
+                const planets = data.map(item => item.name);
+                setPlanets(planets);
+                localStorage.setItem('planets', JSON.stringify({
+                    payload: planets,
+                    timeStamp: Date.now(),
+                }));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-
-
-
-
+    useEffect(() => {
+        const planets = JSON.parse(localStorage.getItem('planets'));
+        if (planets && ((Date.now() - planets.timeStamp) < period_month)) {
+            setPlanets(planets.payload);
+        } else {
+            getPlanetList();
+        }
+    }, [])
 
 
     return (
-        <div>
-            <div className='container'>
-                <div className="row">
-                    <div className="col-25">
-                        <label>First Name</label>
-                    </div>
-                    <div className="col-75">
-                        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-25">
-                        <label htmlFor="lname">Last Name</label>
-                    </div>
-                    <div className="col-75">
-                        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-25">
-                        <label htmlFor="country">Country</label>
-                    </div>
-                    <div className="col-75">
-                        <select id="country" name="country">
-                            <option value="australia">Australia</option>
-                            <option value="canada">Canada</option>
-                            <option value="usa">USA</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-25">
-                        <label htmlFor="subject">Subject</label>
-                    </div>
-                    <div className="col-75">
-                        <textarea id="subject" name="subject" placeholder="Write something.."
-                                  style={{height: '200px'}}></textarea>
-                    </div>
-                </div>
-                <br/>
-                <div className="row">
-                    <input type="submit" value="Submit"/>
-                </div>
-
-            </div>
-        </div>
+        <form className='container' onSubmit={e => {
+            e.preventDefault()
+        }}>
+            <label> First Name:
+                <input type='text' name='firstName' placeholder='Your name...'/>
+            </label>
+            <label> Last Name:
+                <input type='text' name='lastName' placeholder='Your last name...'/>
+            </label>
+            <label> Planet
+                <select name='planet'>
+                    {planets.map(item => <option value={item} key={item}>{item}</option>)}
+                </select>
+            </label>
+            <label> Subject:
+                <textarea name='subject' placeholder='Write something...'></textarea>
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     );
 };
 
